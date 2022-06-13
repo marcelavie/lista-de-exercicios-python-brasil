3496130c0eb5033e4110133619877e44cd9fa293
Marcela Lima <marcelalima.df@gmail.com>
2022-06-13 15:36:01 -0300

    launch

--- secao_03_estrutura_de_repeticao/ex_11_gerar_numeros_de_intervalo_e_somar.py
+++ secao_03_estrutura_de_repeticao/ex_11_gerar_numeros_de_intervalo_e_somar.py
@@ -18,6 +18,4 @@ Também mostre a soma dos números da sequência.
 
 def calcular_numeros_no_intervalo_e_somar(inicio: int, fim: int) -> str:
     """Escreva aqui em baixo a sua solução"""
-    numeros = list(range(inicio, fim))
-    soma = sum(numeros)
-    print(f'', '.join(map(str, numeros)). Soma: (soma)')
+    
--- secao_03_estrutura_de_repeticao/ex_13_potenciacao.py
+++ secao_03_estrutura_de_repeticao/ex_13_potenciacao.py
@@ -18,3 +18,8 @@ Não utilize a função de potência da linguagem.
 
 def calcular_potencia(base: int, expoente: int) -> int:
     """Escreva aqui em baixo a sua solução"""
+    potencia = 1
+    for i in range(expoente):
+        potencia*=base
+        i += 1
+    return potencia
